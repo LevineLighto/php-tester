3a0407d241770e83629d8ba5caf4ae54f57184f5
LevineLighto <ahmad.dlomiri00@gmail.com>
2026-05-14 01:11:28 +0700

    feat(Storage): Add storage class

---
+++ src/Storage/Storage.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Levinelighto\PhpTester\Storage;
+
+class Storage
+{
+    public static function create(string $path, mixed $data)
+    {
+        if (!static::directoryExists(dirname($path))) {
+            static::createDirectory(dirname($path));       
+        }
+
+        $path = static::path($path);
+        file_put_contents($path, $data);
+    }
+
+    public static function update(string $path, mixed $data)
+    {
+        if (!static::directoryExists(dirname($path))) {
+            static::createDirectory(dirname($path));       
+        }
+
+        $path = static::path($path);
+        file_put_contents($path, $data, FILE_APPEND);
+    }
+
+    public static function delete(string $path)
+    {
+        if (!static::exists($path)) {
+            return;
+        }
+        
+        $path = static::path($path);
+        if (!is_file($path)) {
+            return;
+        }
+
+        unlink($path);
+    }
+
+    public static function createDirectory(string $path)
+    {
+        if (static::directoryExists($path)) {
+            return;
+        }
+
+        $path = static::path($path);
+        mkdir($path, 0777, true);
+    }
+
+    public static function exists(string $path)
+    {
+        return file_exists(static::path($path));
+    }
+
+    public static function directoryExists(string $path)
+    {
+        $path = static::path($path);
+        return file_exists($path) && is_dir($path);
+    }
+
+    public static function path(string $path)
+    {
+        return base_path("storage/{$path}");
+    }
+}
